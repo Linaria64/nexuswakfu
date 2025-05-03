abf31438fa5d04cd9cecb91ce32091ae0ed5c5d1
Linaria64 <dorianpro77@proton.me>
2025-05-03 21:05:10 +0200

    backup propre nettoyage complet du code

--- src/lib/utils/debounce.ts
+++
@@ -1,8 +0,0 @@
-// Utilitaire debounce générique
-export function debounce<T extends (...args: any[]) => void>(fn: T, delay = 200): (...args: Parameters<T>) => void {
-  let timeout: ReturnType<typeof setTimeout>;
-  return (...args: Parameters<T>) => {
-    clearTimeout(timeout);
-    timeout = setTimeout(() => fn(...args), delay);
-  };
-}
